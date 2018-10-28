83c808140733233b3e7c69eaa64a1e2988e2e3c8
Ersel Aker <erselaker@gmail.com>
2018-10-28 07:03:17 +0000

    see journeys

--- src/screens/getJourneys/index.js
+++ src/screens/getJourneys/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 import {
   List, ListItem, Body, Right, Button,
 } from 'native-base';
 import moment from 'moment';
 import axios from 'axios';
+import get from 'lodash.get';
+import plur from 'plur';
 import stations from '../../data/stations';
 
 const getFriendlyJourneyLabel = (startingStation, endingStation) => {
@@ -47,22 +49,50 @@ class GetJourneys extends React.Component {
 
   render() {
     const { journeys } = this.state;
+
     return (
-      <List>
-        {journeys.map(journey => (
-          <ListItem key={journey._id}>
-            <Body>
-              <Text>{getFriendlyJourneyLabel(journey.start, journey.end)}</Text>
-              <Text note numberOfLines={2}>{getFriendlyTimeLabel(journey.time)}</Text>
-            </Body>
-            <Right>
-              <Button transparent>
-                <Text>Late by 10mins</Text>
-              </Button>
-            </Right>
-          </ListItem>
-        ))}
-      </List>
+      <ScrollView>
+        <List>
+          {journeys.map((journey) => {
+            const aimedDeparture = get(journey, 'departures.all[0].aimed_departure_time');
+            const expectedDeparture = get(journey, 'departures.all[0].expected_departure_time');
+            let status = '';
+
+            if (aimedDeparture && expectedDeparture) {
+              const aimedTime = new moment(aimedDeparture, 'HH:mm');
+              const expectedTime = new moment(expectedDeparture, 'HH:mm');
+
+              if (aimedTime.isBefore(expectedTime)) {
+              // early
+                status = 'On Time';
+              } else if (aimedTime.isSame(expectedTime)) {
+              // on time
+                status = 'On Time';
+              } else {
+              // late
+                const mins = Math.round(Math.abs(aimedTime.diff(expectedTime) / 60 / 1000));
+                status = `'${mins}${plur('min', 'mins', mins)}`;
+              }
+            }
+
+            return (
+              <ListItem
+                key={journey._id}
+              >
+                <Body>
+                  <Text>{getFriendlyJourneyLabel(journey.start, journey.end)}</Text>
+                  <Text note numberOfLines={2}>{getFriendlyTimeLabel(journey.time)}</Text>
+                </Body>
+                <Right>
+                  <Button transparent>
+                    <Text>{status}</Text>
+                  </Button>
+                </Right>
+              </ListItem>
+            );
+          })}
+        </List>
+      </ScrollView>
     );
   }
 }
